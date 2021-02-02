ade2e057b572806e6ac6dc6fc7f32aeeb98f9d92
Grégory Doucet <doucetgregoryperso@outlook.fr>
2021-02-02 09:21:41 +0100

    show classement + update

--- tp.py
+++ tp.py
@@ -2,6 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import mplcursors
+import operator
 
 all_months = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre']
 data = pd.read_excel('Climat.xlsx','SI ',usecols=range(3,15),skiprows=4,skipfooter=17,header=None)
@@ -32,11 +33,10 @@ europeData['AvgTemperature'] = (europeData['AvgTemperature'] - 32) / 1.8
 
 
 def correctData():
-   for value in data :
-       for i in range(len(data)) :
-           if type(data[value][i])==str:
-               data[value][i]=(data[value][i-1]+data[value][i+1])/2
-   
+    for value in data :
+        for i in range(len(data)) :
+            if type(data[value][i])==str:
+                data[value][i]=(data[value][i-1]+data[value][i+1])/2
 
 
 def findAverage(month):
@@ -88,38 +88,37 @@ def findCapital():
         if (sumDif[x] < maxDif):
             maxDif = sumDif[x]
             cityFound = x
-    return cityFound
+    return sorted(sumDif.items(), key=operator.itemgetter(1,0))
 
 def menu():
-      while True:
-
-            try:
-                result = int(input("Que veux tu savoir: 1-moyenne/écart_type/min/max  2-diagramme 3-Capitale 4-exit : " ))
-            except ValueError:
-                print("Error! This is not a number. Try again.")
-            if(result==1):
-                month = input('Quelle est le mois souhaitez ?')
-                print(findAverage(month))
-            if(result==2):
-                menuYear()
-            if(result==3):
-                print(findCapital())
-            else:
-                break
+    while True:
+        try:
+            result = int(input("Que veux tu savoir: 1- Moyenne/écart_type/min/max  2- Diagramme 3- Capitale 4- Exit : " ))
+        except ValueError:
+            print("Error! This is not a number. Try again.")
+        if (result == 1):
+            month = input('Quelle est le mois souhaitez ?')
+            print(findAverage(month))
+        if (result == 2):
+            menuYear()
+        if (result == 3):
+            print(findCapital())
+        if (result == 4):
+            break
 # The function is called
 
 def menuYear():   
-            try:
-                result = int(input("Quelle diagramme veux tu: 1-Un mois 2-Tous les mois  3-Par Année 4-exit : " ))
-            except ValueError:
-                print("Error! This is not a number. Try again.")
-            if(result==1):
-                month = input('Quelle est le mois souhaitez ?')
-                createCurvesByMonth(month)
-            if(result==2):
-                createCurvesAllMonth()
-            if(result==3):
-                createCurvesByYear()
+    try:
+        result = int(input("Quelle diagramme veux tu: 1- Un mois 2- Tous les mois  3- Par Année 4- Exit : " ))
+    except ValueError:
+        print("Error! This is not a number. Try again.")
+    if (result == 1):
+        month = input('Quelle est le mois souhaitez ?')
+        createCurvesByMonth(month)
+    if (result == 2):
+        createCurvesAllMonth()
+    if (result == 3):
+        createCurvesByYear()
 
 menu()
 
